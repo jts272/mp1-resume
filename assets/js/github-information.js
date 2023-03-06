83c335267bd37386e9ef8144cf3bb83579fbfeb5
jts272 <jts272@outlook.com>
2023-03-06 11:17:52 +0000

    Code promise for getting JSON data from GitHub API

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -13,4 +13,26 @@ function fetchGitHubInformation(event) {
     <img src="assets/css/loader.gif" alt="Loading..." />
   </div>
   `);
+
+  // Start of jQuery promise:
+  // when() arg: getJSON
+  // then() args: onSuccessfulResponse, onErrorResponse
+  $.when($.getJSON(`https://api.github.com/users/${username}`)).then(
+    function (response) {
+      const userData = response;
+      $("#gh-user-data").html(userInformationHTML(userData));
+    },
+    function (errorResponse) {
+      if (errorResponse.status === 404) {
+        $("#gh-user-data").html(`
+        <h2>No info found for user ${username}</h2>
+        `);
+      } else {
+        console.log(errorResponse);
+        $("gh-user-data").html(`
+        <h2>Error: ${errorResponse.responseJSON.message}</h2>
+        `);
+      }
+    }
+  );
 }
